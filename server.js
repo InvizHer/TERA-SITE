e134c7d77b8098a1109bd4cd12a222c44e418ab5
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-05-20 12:17:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID;
+const MONGO_URI = "mongodb+srv://codexun:TeamCodexun07@codexun.egmx5.mongodb.net/?retryWrites=true&w=majority";
+const TELEGRAM_BOT_TOKEN = "6951265744:AAFbjyOh4wjIGqx4yRPgdEJp1MWgvf3MUZI";
+const TELEGRAM_CHANNEL_ID = "-1001580057771";
 
 // Movie Schema
 const movieSchema = new mongoose.Schema({
